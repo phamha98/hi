b07088fbdb80cb78496ffbfa96367cb42c478497
Phạm Trần Quang Hà <78589034+phamha98@users.noreply.github.com>
2022-03-25 09:13:45 +0700

    obb js

---
+++ Js/Encapsulation.java
@@ -0,0 +1,30 @@
+class Student {
+    private String name;
+    private int age;
+    private double gpa;
+ 
+    public String getName() {
+       return name;
+    }
+ 
+    public void setName(String name) {
+       this.name = name;
+    }
+ 
+    public int getAge() {
+       return age;
+    }
+ 
+    public void setAge(int age) {
+       this.age = age;
+    }
+ 
+    public double getGpa() {
+       return gpa;
+    }
+ 
+    public void setGpa(double gpa) {
+       this.gpa = gpa;
+    }
+ 
+ }

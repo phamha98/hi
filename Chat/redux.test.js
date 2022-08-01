d7028b4c2a75d935965b6b590c17bae2c840ef57
Phạm Trần Quang Hà <78589034+phamha98@users.noreply.github.com>
2022-08-01 07:23:20 +0700

    mm

--- Chat/redux.test.js
+++
@@ -1,9 +0,0 @@
-const s={
-    a:12
-}
-const r ={
-    ...s,
-    b:123
-}
-
-const s=r.a
--- SVG_convert_React/svg/index.js
+++
@@ -1,3 +0,0 @@
-export {default as Icon_back} from './src/back.svg'
-export {default as Icon_drop_down} from './src/drop_down.svg'
-

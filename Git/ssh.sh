2e40b4884eb34cd28721da03c64817044dbc0ed2
Phạm Trần Quang Hà <78589034+phamha98@users.noreply.github.com>
2023-10-25 10:10:01 +0700

    add

---
+++ Git/ssh.sh
@@ -0,0 +1,8 @@
+cd ~/.ssh && ssh-keygen
+
+Generating public/private rsa key pair.
+Enter file in which to save the key (/Users/phamha/.ssh/id_rsa):company
+
+
+ssh-add ~/.ssh/id_rsacompany
+cat id_rsacompany.pub | pbcopy
